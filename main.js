894a72ca013597c759052e667b2cf0affbccf021
Viktoriia Kleiko <vika.kleyko@gmail.com>
2020-06-23 11:49:50 -0700

    move createEl outside the DomElement classs

--- main.js
+++ main.js
@@ -8,26 +8,25 @@ function DomElement(selector, height, width, bg, fontSize) {
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
-
-    DomElement.prototype.createEl = function () {
-         if (this.selector.substring(0, 1) === ".") {
-            domEl = document.createElement("div");
-            domEl.classList.add(this.selector.substring(1));
-        }
-        if (this.selector.substring(0, 1) === "#") {
-            domEl = document.createElement("p");
-            domEl.id = this.selector.substring(1);
-        }
-        domEl.style.width = this.width;
-        domEl.style.height = this.height;
-        domEl.style.background = this.bg;
-        domEl.style.fontSize = this.fontSize;
-        domEl.textContent = "Hi there";
-        
-        document.body.append(domEl);
-    };
 }
 
+DomElement.prototype.createEl = function () {
+    if (this.selector.substring(0, 1) === ".") {
+       domEl = document.createElement("div");
+       domEl.classList.add(this.selector.substring(1));
+   }
+   if (this.selector.substring(0, 1) === "#") {
+       domEl = document.createElement("p");
+       domEl.id = this.selector.substring(1);
+   }
+   domEl.style.width = this.width;
+   domEl.style.height = this.height;
+   domEl.style.background = this.bg;
+   domEl.style.fontSize = this.fontSize;
+   domEl.textContent = "Hi there";
+   
+   document.body.append(domEl);
+};
 
 let elem = new DomElement("#best", "150px", "350px", "yellow", "16px");
 
